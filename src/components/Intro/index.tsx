8bc3b16d93c214baa105373b9c427108114ab2b7
Adrian Faustino <contact.adrianfaustino@gmail.com>
2024-06-24 15:40:06 -0700

    Add Intro component scaffolding

--- src/components/Intro/index.tsx
+++ src/components/Intro/index.tsx
@@ -1,5 +1,46 @@
+import { USER_DATA } from "@/db";
+
 interface IntroProps {}
 
 export function Intro({}: IntroProps) {
-  return <div>Intro</div>;
+  const {
+    first_name,
+    last_name,
+    title,
+    description,
+    location,
+    avatar_url,
+    resume_url,
+    linkedin_url,
+    github_url,
+  } = USER_DATA;
+
+  return (
+    <div>
+      <div>
+        {/* Avatar */}
+        <div>AVTR PLACEHOLDER</div>
+
+        {/* Title, Location... */}
+        <div>
+          <h2>
+            {first_name} {last_name}
+          </h2>
+
+          <h3>{title}</h3>
+
+          <div>
+            <span>{location}</span>
+
+            <span>{linkedin_url}</span>
+
+            <span>{github_url}</span>
+          </div>
+        </div>
+      </div>
+
+      {/* Desc */}
+      <div>{description}</div>
+    </div>
+  );
 }
---
+++ src/db/index.ts
@@ -0,0 +1,15 @@
+import { User } from "@/app/types";
+
+// Refactor: persist in ext DB
+export const USER_DATA: User = {
+  first_name: "Adrian",
+  last_name: "Faustino",
+  title: "Software Developer",
+  description:
+    "Experienced Software Developer based in Vancouver, with a proven track record in both small to mid-sized startups and at tech giant Amazon.com.",
+  location: "Vancouver, BC",
+  avatar_url: "TODO",
+  resume_url: "TODO",
+  linkedin_url: "https://www.linkedin.com/in/adrianfaustino",
+  github_url: "https://github.com/adrian-faustino",
+};
